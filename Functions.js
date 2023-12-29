28b2049d0c0c87368863d168c6cdaf0e5a9199cb
vipulc2580 <vipulc2580@gmail.com>
2023-12-29 23:43:47 +0530

    learnt fuctions

--- Functions.js
+++ Functions.js
@@ -1,10 +1,10 @@
-console.log("We will learn fuctions today");
+// console.log("We will learn fuctions today");
 // Function is JS - It is a block code that performs a specific task,can be invoked whenever needed
 // functions help you achieve Dry(dont repeat yourself) write once and use whenever we want
 // Functions helps remove redundancy from our code
 
 
-console.log("hello World".toUpperCase());
+// console.log("hello World".toUpperCase());
 // here to UpperCase() is nothing but a function whose task to convert given string to UpperCase by creating a new one and returning new
 // Uppercase string
 // Infact console.log() log() it self is a function
@@ -28,13 +28,13 @@ console.log("hello World".toUpperCase());
 
 // Take user Input and print his userName using a function which takes the name as input
 
-function displayUserId (name){
-    //  parameter is nothing but input to our function
-    console.log(`Welcome ${name} your UserId is ${'@'+name+name.length}`);
-}
+// function displayUserId (name){
+//     //  parameter is nothing but input to our function
+//     console.log(`Welcome ${name} your UserId is ${'@'+name+name.length}`);
+// }
 
-let name =prompt("Enter your name : ");
-displayUserId(name);
+// let name =prompt("Enter your name : ");
+// displayUserId(name);
 // name passed to function is called argument which acts as parameter for the called function
 // variable used in function definition is called parameter and variable passed in function call is called parameter
 
@@ -101,4 +101,65 @@ console.log(arrowSub);
 arrowSub=5;
 console.log(arrowSub);
 // it prints 5 as it is reasssigned hence we make function variable to be const so that it can not be reassignmeed later
+
+
+// Arrow Function are usually used for function which has less work to done ex:addition printing
+// if there is just single instruction within arrowFunction we can remove curly braces as well
+
+const printMsg=(msg)=> console.log(msg);
+
+
+
+// q1 create a function to take string as argument and returns number of vowels in it
+
+function countVowel(sentence){
+    let counter=0;
+    for(let a of sentence){
+        if(a==='a' || a==='A' || a==='e' || a==='E' || a==='i'|| a==='I' || a==='o' || a==='O' || a==='u'|| a==='U') counter++;
+    }
+    return counter;
+}
+
+
+
+console.log("Vowel count in given sentence is : ",countVowel("bhavani plywood and hardware"));
+
+function sum(b,a){
+    console.log("i m second function with two parameters");
+}
+// doing the same using arrow function
+
+// const countVowel=(msg)=>{
+//     let counter=0;
+//     for(let a of sentence){
+//         if(a==='a' || a==='A' || a==='e' || a==='E' || a==='i'|| a==='I' || a==='o' || a==='O' || a==='u'|| a==='U') counter++;
+//     }
+//     return counter;
+// }
+
+// const countVowel=(msg,n)=>{
+//     console.log("It works");
+// }
+function sum(){
+    console.log(" I m function with no parameters");
+}
+function sum(a){
+    console.log("Im first function");
+}
+
+// JavaScript doesnt supports method overloading 
+
+// Nan as output means Not A Number in short not valid operation
+// Return statement can only return one value which can be a complex obj but only one value
+// Statements after return statement are not executed as return statement just returns the value to caller function and passes the con
+// trol over it
+
+let mul=(a,b)=>{
+    return a*b;
+}
+
+console.log(mul(8,9));
+
+
+
 
